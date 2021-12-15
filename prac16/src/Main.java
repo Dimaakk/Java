52a334f97b67d60b2756c13ddf82df760a82fda8
Dmitry <94452191+Dimaakk@users.noreply.github.com>
2021-12-15 21:00:04 +0300

    Add files via upload

---
+++ prac16/src/Main.java
@@ -0,0 +1,56 @@
+
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main
+{
+    private static String staffFile = "data/staff.txt";
+    private static String dateFormat = "dd.MM.yyyy";
+
+    public static void main(String[] args)
+    {
+        ArrayList<Employee> staff = loadStaffFromFile();
+        staff.sort(((o1, o2) -> {
+            int res = (int) (o1.getSalary() - o2.getSalary());
+            if (res != 0)
+                return res;
+            else
+                return o1.getName().compareTo(o2.getName());
+        }));
+
+        for (Employee st : staff) {
+            System.out.println(st);
+        }
+    }
+
+    private static ArrayList<Employee> loadStaffFromFile()
+    {
+        ArrayList<Employee> staff = new ArrayList<>();
+        try
+        {
+            List<String> lines = Files.readAllLines(Paths.get(staffFile));
+            for(String line : lines)
+            {
+                String[] fragments = line.split("\t");
+                if(fragments.length != 3) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                staff.add(new Employee(
+                        fragments[0],
+                        Integer.parseInt(fragments[1]),
+                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                ));
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return staff;
+    }
+}
